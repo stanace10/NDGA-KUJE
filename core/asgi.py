9585df7f5a0da6f788d96300150f0a040a681302
stanace10 <stanace10@users.noreply.github.com>
2026-03-09 13:48:07 +0100

    Fix ASGI startup order

--- core/asgi.py
+++ core/asgi.py
@@ -1,14 +1,15 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-from core.routing import websocket_urlpatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from core.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
